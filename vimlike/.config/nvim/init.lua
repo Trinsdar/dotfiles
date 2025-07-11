9e519bc14d22c1d2edd1bd271291a4a0cfa34cfb
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-07-11 07:26:40 -0400

    installed lazyvim

---
+++ vimlike/.config/nvim/init.lua
@@ -0,0 +1,7 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.cmd("source ~/.vimrc")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
