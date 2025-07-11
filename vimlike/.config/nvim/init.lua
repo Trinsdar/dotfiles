0cadebbf3729f01c9f9f8a18c7d82d9ad3985907
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2025-07-11 09:58:18 -0400

    removed tab lines since they are defined in stylua.toml

--- vimlike/.config/nvim/init.lua
+++ vimlike/.config/nvim/init.lua
@@ -1,7 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 vim.cmd("source ~/.vimrc")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
