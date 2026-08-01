4a2ef56976395c93eb1bb9bfd8fd3e3ab93cd980
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2026-07-31 22:55:28 -0400

    added move to workspace silent keybind

--- hypr/.config/hypr/dms/binds-desktop.lua
+++ hypr/.config/hypr/dms/binds-desktop.lua
@@ -1,5 +1,6 @@
 for i = 1, 10 do
 	local key = i % 10 -- 10 maps to key 0
 	hl.bind("SUPER + " .. key, hs.dsp.focus({ workspace = i }))
-	hl.bind("SUPER + SHIFT + " .. key, hs.dsp.window.move({ workspace = i, follow = true }))
+	hl.bind("SUPER + SHIFT + " .. key, hs.dsp.window.move({ workspace = i }))
+	hl.bind("SUPER + SHIFT + CTRL + " .. key, hs.dsp.window.move({ workspace = i, follow = false }))
 end
--- hypr/.config/hypr/dms/binds-user.lua
+++ hypr/.config/hypr/dms/binds-user.lua
@@ -108,11 +108,12 @@ hl.bind(mainMod .. " + CTRL + SHIFT + R", hl.dsp.exec_cmd("dms ipc call workspac
 
 -- Switch workspaces with mainMod + [0-9]
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
-if string.find(getHostname(), "thinkpad") then
+if isLaptop then
 	for i = 1, 10 do
 		local key = i % 10 -- 10 maps to key 0
 		hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
 		hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
+		hl.bind(mainMod .. " + SHIFT + CTRL + " .. key, hl.dsp.window.move({ workspace = i, follow = false }))
 	end
 else
 	require("dms.binds-desktop")
